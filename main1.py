2065c40a4412b6eff342553628adee04f74730dd
Vinkgr2 <103883992+Vinkgr2@users.noreply.github.com>
2022-04-17 16:32:33 +0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,16 @@
+﻿from direct.showbase.ShowBase import ShowBase
+ 
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.model = loader.loadModel('models/environment')
+        self.model.reparentTo(render)
+        self.model.setScale(0.1)
+        self.model.setPos(-2, 25, -3)
+        base.disableMouse()
+        base.camera.setPos(base.camera, 6, 4, 5)
+        base.camera.setHpr(35, 15, 45)
+ 
+game = Game()
+game.run()
+
